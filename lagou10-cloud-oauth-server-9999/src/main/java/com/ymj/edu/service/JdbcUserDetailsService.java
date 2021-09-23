e1fc023847a446b31c47343da9720284d4de4c82
Asurazzz <2513667783@qq.com>
2021-09-23 16:29:00 +0800

    测试从数据库验证⽤户合法性

---
+++ lagou10-cloud-oauth-server-9999/src/main/java/com/ymj/edu/service/JdbcUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.ymj.edu.service;
+
+import com.lagou.edu.pojo.Users;
+import com.ymj.edu.dao.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class JdbcUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    /** 根据username查询出该⽤户的所有信息，封装成UserDetails类型的对象返回，⾄于密码，框架会⾃动匹配
+     * @param username
+     * @return
+     * @throws UsernameNotFoundException
+    */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws
+            UsernameNotFoundException {
+        Users users = usersRepository.findByUsername(username);
+        return new User(users.getUsername(),users.getPassword(),new ArrayList<>());
+    }
+
+}
