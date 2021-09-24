a43ea74f020a1599bd4ed71398fff8778af11a68
Asurazzz <2513667783@qq.com>
2021-09-24 17:10:42 +0800

    oauth修改

--- lagou10-cloud-oauth-server-9999/src/main/java/com/ymj/edu/dao/UsersRepository.java
+++ lagou10-cloud-oauth-server-9999/src/main/java/com/ymj/edu/dao/UsersRepository.java
@@ -4,5 +4,6 @@ import com.lagou.edu.pojo.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersRepository extends JpaRepository<Users, Long> {
+
     Users findByUsername(String username);
 }
